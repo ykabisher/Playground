cde8f7affa27e45bfaa8ee2543be7b415e2ee830
Yevgeni Kabisher <yevgeni.kabishcher@gmail.com>
2026-03-12 21:59:03 +0200

    Add GitHub Actions workflow for deploying to GitHub Pages and update Vite config for base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.VITE_BASE ?? "/",
   server: {
     port: 5173,
-    open: false
-  }
+    open: false,
+  },
 });
